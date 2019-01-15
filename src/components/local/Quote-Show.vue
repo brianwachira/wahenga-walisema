ed6ed44021393e157ba146054c9c83194a333a35
brianwachira <brianwachira7@gmail.com>
2019-01-15 23:54:40 +0300

    make inital commit + connect serverBus to created method.big things yow:)

---
+++ src/components/local/Quote-Show.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi pariatur molestiae, voluptate nulla doloremque est labore at. Unde nesciunt doloribus error corporis? Quaerat tenetur exercitationem quisquam, corrupti asperiores minima eum!Lorem</p>
+</template>
+<script>
+    import { serverBus } from '../../main';
+    export default {
+     data(){
+         return {
+             quote: null
+         }
+     },
+     methods:{
+         created(){
+             serverBus.$on('quote',(quote)=>{
+                 this.quote = quote;
+             });
+         }
+     }
+    }
+</script>
+
