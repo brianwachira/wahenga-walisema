8310ad8f507112a3fc001f6f44dbd89992c61835
brianwachira <brianwachira7@gmail.com>
2019-01-15 18:53:15 +0300

    add head content

---
+++ src/components/shared/Head.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <h1 class="text-center text-light pt-5">Wahenga Walisema</h1>
+    <h2 class="text-light ml-5"> Light fires in people's hearts</h2>
+</div>
+</template>
+<style scoped>
+h1{
+    font-family: 'ZCOOL KuaiLe', cursive;
+}
+h2{
+    font-family: 'Indie Flower', cursive;
+}
+</style>
+
