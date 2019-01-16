78428a681178af7ff0c1d02988afea71e6b4fdb2
brianwachira <brianwachira7@gmail.com>
2019-01-16 16:11:15 +0300

    make template elements responsive

--- src/components/local/Quotes.vue
+++ src/components/local/Quotes.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-    <button class="btn btn-primary-outline indie mt-5" v-show="!addQuote" @click="addQuote = !addQuote">Add a quote</button><br/>
-    <div v-show="addQuote">
-        <label class="lbl text-white indie mt-3" for="author">Author</label>
-        <input type="text" class="form-control indie w-75" name="author" v-model="quote.author" placeholder="Enter the name of the author" required/>
-        <label class="lbl text-white indie mt-3" for="author ">Quote</label>
-        <textarea name="quote" class="form-control indie w-75" cols="30" rows="4"  v-model="quote.content" placeholder="Enter a quote" required></textarea>
-        <button class="btn btn-primary-outline indie mt-5" @click="displayQuote()">submit it!</button><br/>
-    </div>
+        <div v-show="!addQuote">
+            <button class="btn btn-primary-outline indie mt-5 mx-auto d-block"  @click="addQuote = !addQuote">Add a quote</button><br/>
+        </div>
+        <div v-show="addQuote">
+            <label class=" lbl text-white indie mt-3" for="author">Author</label>
+            <input type="text" class="form-control indie w-75  mx-auto d-block" name="author" v-model="quote.author" placeholder="Enter the name of the author" required/>
+            <label class="lbl text-white indie mt-3  mx-auto" for="author">Quote</label>
+            <textarea name="quote" class="form-control indie w-75 mx-auto d-block" cols="30" rows="4"  v-model="quote.content" placeholder="Enter a quote" required></textarea>
+            <button class="btn btn-primary-outline indie mt-5 mx-auto d-block" @click="displayQuote()">submit it!</button><br/>
+        </div>
     </div>
 </template>
 <script>
@@ -25,8 +27,6 @@
         },
         methods : {
             displayQuote(){
-                //console.log(this.quote.author);
-                //console.log(this.quote.content);
                 this.quotePackage.unshift({
                     author : this.quote.author,
                     content : this.quote.content
@@ -50,6 +50,9 @@ button{
 button:hover{
     background-color: white;
     color: black;
+}
+d-inherit{
+    display: inherit
 }
 </style>
 
